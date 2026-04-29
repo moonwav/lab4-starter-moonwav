1245676fd1c86b8a5ae829efc0958ba3c7ae7ea0
moonwav <158658023+moonwav@users.noreply.github.com>
2026-04-28 22:59:17 -0700

    Fixed

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,6 +7,9 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
+        if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+            return false;
+        }
         let result = 0;
 
         result = num1 + num2;
@@ -25,7 +28,7 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    if (prices.length === 0) {
+    if (!Array.isArray(prices) || prices.length === 0 || typeof discount !== 'number') {
         return false;
     }
     const discounted = []
